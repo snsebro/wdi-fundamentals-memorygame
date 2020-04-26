34d0cecb8d61907e069ebd2186e398838d39b669
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-04-25 21:41:44 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,19 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne)
-console.log("User flipped " + cardOne);
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert ("theres a match")
-} else { alert ("not a match")
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!")
+} else {
+    console.log("Sorry, try again.")
+}
 };
 
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
 
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardThree);
+flipCard(0);
+flipCard(2);
